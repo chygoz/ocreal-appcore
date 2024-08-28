42536f432f68ae23dd9957d72e621d6a9bc160af
James Iweobi <iweobijames@gmail.com>
2024-08-28 20:21:14 +0100

    Setup cors for prod

--- src/main.ts
+++ src/main.ts
@@ -20,10 +20,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
-  if (
-    process.env.NODE_ENV === 'development' ||
-    process.env.NODE_ENV === 'production'
-  ) {
+  if (process.env.NODE_ENV === 'development') {
     app.use((req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
