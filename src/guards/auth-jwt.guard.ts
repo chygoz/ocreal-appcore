06825f253bcc185be5046751dc412def5edca469
gbubemi22 <slimnfine22@gmail.com>
2024-11-18 11:14:34 +0100

    guard

--- src/guards/auth-jwt.guard.ts
+++ src/guards/auth-jwt.guard.ts
@@ -9,7 +9,6 @@ import { Request } from 'express';
 import { decodeJwtToken } from 'src/utils/jwt.util';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { AccountTypeEnum } from 'src/constants';
 import { User } from 'src/modules/users/schema/user.schema';
 
 declare global {
@@ -17,44 +16,34 @@ declare global {
   namespace Express {
     interface Request {
       user?: User;
-      active_user_role: AccountTypeEnum;
     }
   }
 }
+
 export class JwtAuthGuard implements CanActivate {
   constructor(
     @InjectModel(User.name) private readonly userModel: Model<User>,
   ) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-
     return this.validateRequest(request);
   }
 
   async validateRequest(request: Request): Promise<boolean> {
     if (!request.headers.authorization) {
       throw new BadRequestException(
-        'Please provde bearer token in authorization header.',
+        'Please provide bearer token in authorization header.',
       );
     }
-    if (!request.headers.role) {
-      throw new BadRequestException('Please provide an role in the header.');
-    }
-    const inclduded = Object.values(AccountTypeEnum).includes(
-      request.headers.role as AccountTypeEnum,
-    );
-    if (!inclduded) {
-      throw new BadRequestException(
-        'Please provide a valid user role as role in the header.',
-      );
-    }
+
     const token = request.headers.authorization.split(' ')[1];
 
     if (!token) {
       throw new BadRequestException(
-        'Please provde bearer token in authorization header.',
+        'Please provide bearer token in authorization header.',
       );
     }
 
@@ -64,13 +53,11 @@ export class JwtAuthGuard implements CanActivate {
     }
 
     const user = await this.userModel.findById(decodedToken.id);
-
     if (!user) {
       throw new UnauthorizedException('Invalid token. Please login again.');
     }
-    request['user'] = user;
-    request['active_user_role'] = request.headers.role as AccountTypeEnum;
 
+    request['user'] = user;
     return true;
   }
 }
